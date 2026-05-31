d3795c76fe26d6324c78343fb6012d3b23a98213
noizzze2 <noizzze20@gmail.com>
2026-05-31 11:58:30 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,22 @@
-// Copyright 2021 NNTU-CS
+// Copyright 2025 NNTU-CS
+#include <iostream>
+#include <fstream>
 #include "bst.h"
 
+void makeTree(BST<std::string>& tree, const char* filename);
+void printFreq(BST<std::string>& tree);
+
 int main() {
-    return 0;
+  BST<std::string> tree;
+  makeTree(tree, "src/war_peace.txt");
+
+  std::ofstream out("result/freq.txt");
+  auto freq = tree.getFreq();
+  for (const auto& p : freq) {
+    out << p.first << " " << p.second << std::endl;
+  }
+  out.close();
+
+  printFreq(tree);
+  return 0;
 }
